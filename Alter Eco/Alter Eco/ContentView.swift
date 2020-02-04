f305ced978a21a362edddb25d9af923ac329f359
ewithnell <ew1213@ic.ac.uk>
2020-02-04 10:53:55 +0000

    Update Main ContentView file.

--- Alter Eco/Alter Eco/ContentView.swift
+++ Alter Eco/Alter Eco/ContentView.swift
@@ -1,26 +1,129 @@
 //
-//  ContentView.swift
-//  Alter Eco
+//  View2.swift
+//  TrackerGraphs2UI
 //
-//  Created by Satisfaction on 30/01/2020.
-//  Copyright © 2020 Imperial College London. All rights reserved.
+//  Created by e withnell on 19/01/2020.
+//  Copyright © 2020 e withnell. All rights reserved.
 //
 
+// This is to try and commit
+
+//import Foundation
 import SwiftUI
-import CoreMotion
+//MAX'S HOMESCREEN
+
+import MapKit
 
 struct ContentView: View {
-    
-    var body: some View {
-        
-        Text("Hello World, this is our new Git Repo!")
+  @State var showSplash = true
+  
+  var body: some View {
+    ZStack{
+      
+          DetailView()
+      SplashScreen()
+        .opacity(showSplash ? 1 : 0)
+        .onAppear {
+          DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+         
+     
+          //  MapView(coordinate: CLLocationCoordinate2DMake(37.331820, -122.03118))
+            //    .edgesIgnoringSafeArea(.all)
+            SplashScreen.shouldAnimate = false
+            withAnimation() {
+              self.showSplash = false
+           
+            }
+            
+          }
+            
+      }
         
     }
     
+  }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+#endif
+
+
+//                SplashScreen()
+//                    .opacity(self.showSplash ? 1 : 0)
+//                    .onAppear {DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//                        SplashScreen.shouldAnimate = false
+//                        withAnimation() {
+//                            self.showSplash = false
+//                            }
+//                        }
+//                    }
+                
+//        }
+  //  }
+//}
+
+
+//struct ContentView_Previews: PreviewProvider {
+  //  static var previews: some View {
+    //    ContentView()
+    //}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PREVIOUS BUTTON HOME SCREEN:
+
+//struct ContentView: View {
+//
+//    @State private var isActive: Bool=false
+//    @State private var selection: Int? = nil
+//
+//    var body: some View {
+//
+//        NavigationView {
+//
+//      VStack {
+//                NavigationLink(destination: DetailView(), tag: 1, selection: self.$selection) {
+//                    Text("")
+//                }
+//
+//                Button("Woo it's the Carbon Tracker!")
+//                {
+//                self.selection=1
+//            }
+//    }
+//    }
+//}
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+
