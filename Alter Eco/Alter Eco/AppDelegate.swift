fbc7bf62662285289033137a283fb94fadc281fe
Maxime Redstone <mbr19@ic.ac.uk>
2020-02-05 14:49:29 +0000

    App Delegate SQLite

--- Alter Eco/Alter Eco/AppDelegate.swift
+++ Alter Eco/Alter Eco/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreMotion
 
+import SQLite
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
