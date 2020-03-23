d0b556e5ee07bc515a9b9db48dd32330901463f4
MaximeRedstone <maxime.redstone.16@ucl.ac.uk>
2020-03-23 22:53:15 +0100

    Imported state of master branch into develop.

---
+++ Alter Eco/Alter Eco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alter Eco
+//
+//  Created by Satisfaction on 30/01/2020.
+//  Copyright © 2020 Imperial College London. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Hello World, this is our new Git Repo!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
