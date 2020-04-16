6f189908e1e4a43dc1234a66fe36dc04d14dbc68
Michele Asara <ma7914@ic.ac.uk>
2020-04-16 22:27:15 +0100

    Timers and constants documented.

--- Alter Eco/Alter Eco/Backend/Constants.swift
+++ Alter Eco/Alter Eco/Backend/Constants.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 // MARK: - Speed constants
-// define the average speed of the tube in London as 33 kmph converted to 9.17 m/s
+/// Defines the average speed of the tube in London as 33 kmph converted to 9.17 m/s
 public let AVERAGE_TUBE_SPEED:Double = 9.17
-// define average plane speed as (740 - 930) kmph converted to 222 m/s
+/// Defines the average plane speed as (740 - 930) kmph, converted to 222 m/s.
 public let AVERAGE_PLANE_SPEED:Double = 222
 
 // MARK: - MapKit requests constants
-// define radius of search for stations
+/// Defines radius of search for stations.
 public let STATION_REQUEST_RADIUS:Double = 150
-// define radius of search for airports
+/// Defines radius of search for airports.
 public let AIRPORTS_REQUEST_RADIUS:Double = 2500
-// natural language query for train and tube stations nearby
+/// Natural language query for train and tube stations nearby.
 public let QUERY_TRAIN_STATIONS:String = "underground train subway tube station"
-// natural language query for airports nearby
+/// Natural language query for airports nearby.
 public let QUERY_AIRPORTS: String = "airport"
 
 // MARK: - Region-Of-Interest (ROI) activities constants
@@ -21,29 +21,29 @@ public let QUERY_AIRPORTS: String = "airport"
 public let MAX_DISTANCE_WITHIN_AIRPORT:Double = 1000
 // define average radius of station m
 public let MAX_DISTANCE_WITHIN_STATION:Double = 60
-// idle time (in seconds) after which the activity estimator should forget the user was in a station
+/// Idle time (in seconds) after which the activity estimator should forget the user was in a station.
 public let STATION_TIMEOUT:Double = 90*60
-// idle time (in seconds) after which the activity estimator should forget the user was in an airport
+/// Idle time (in seconds) after which the activity estimator should forget the user was in an airport.
 public let AIRPORT_TIMEOUT:Double = 60*60*24
-// used for plane motion type, determines how many measurements of type car needed before resetting airport flag
+/// Determines how many measurements of type car the activity estimator needs before resetting airport flag.
 public let CAR_NUM_FOR_PLANE_FLAG_OFF:Int = 10
-// used for tube motion type, determines how many measurements of type walking needed before resetting train flag
+/// Determines how many measurements of type walking the activity estimator needs before resetting train flag.
 public let WALK_NUM_FOR_TRAIN_FLAG_OFF:Int = 3
 
 // MARK: - Speed-based activities constants
-// define how many measurements in a row must be different from the root measurement before an activity is estimated
+/// Defines how many measurements in a row must be different from the root measurement before an activity is estimated.
 public let CHANGE_ACTIVITY_THRESHOLD:Int = 2
-// defines weights in computing average of speed-based activities
+/// Defines weights in computing average of speed-based activities.
 public let ACTIVITY_WEIGHTS_DICT: [MeasuredActivity.MotionType: Int] = [.car: 2, .walking: 1]
-// defines how many seconds must pass for an activity to expire in the absence of ROI flags
+/// Defines how many seconds must pass for an activity to expire in the absence of ROI flags.
 public let ACTIVITY_TIMEOUT : Double = 30*60
 
 // MARK: - GPS constants
-// defines how many meters to request a gps update
+/// Defines how many meters to request a gps update.
 public let GPS_UPDATE_DISTANCE_THRESHOLD:Double = 50
-// define tolerance value in meters for gps updates
+/// Defines tolerance value in meters for gps updates.
 public let GPS_UPDATE_DISTANCE_TOLERANCE:Double = 5
-// define minimum confidence for valid location updates
+/// Defines the minimum confidence for valid location updates.
 public let GPS_UPDATE_CONFIDENCE_THRESHOLD:Double = 50
-// define area near airport still considered as an airport location - Paris airport 5kmx3km
+/// Defines the area near airport still considered as an airport location - Paris airport is 5kmx3km
 public let GPS_UPDATE_AIRPORT_THRESHOLD:Double = 4000
--- Alter Eco/Alter Eco/Backend/Timers.swift
+++ Alter Eco/Alter Eco/Backend/Timers.swift
@@ -1,18 +1,43 @@
 import Foundation
 
+/// An interface which allows to handle multiple countdowns.
 public protocol CountdownHandler {
+    /**
+     Starts a countdown.
+     - Parameters key: identifier for the countdown.
+     - Parameters interval: how long the countdown will be.
+     - Parameters block: procedure called at the end of the countdown.
+     */
     func start(key: String, interval: TimeInterval, block: @escaping () -> Void)
+    
+    /**
+    Stops a countdown.
+    - Parameters key: identifier for the countdown.
+     */
     func stop(_ key: String)
 }
 
+/// Represents an object associating keys to specific timers.
 public class MultiTimer : CountdownHandler {
     private var timers : [String: Timer] = [:]
     
+    /**
+    Starts a countdown.
+    - Parameters key: identifier for the countdown.
+    - Parameters interval: how long the countdown will be.
+    - Parameters block: procedure called at the end of the countdown.
+    - Remark: if start is called with a key already associated to a countdown, the latter is stopped and restarted.
+    */
     public func start(key: String, interval: TimeInterval, block: @escaping () -> Void) {
         stop(key)
         timers[key] = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(callback(timer:)), userInfo: block, repeats: false)
     }
     
+    /**
+    Stops a countdown.
+    - Parameters key: identifier for the countdown.
+    - Remark: if no countdown is associated with the given key, nothing happens.
+     */
     public func stop(_ key: String) {
         if let timer = timers[key] {
             timer.invalidate()
