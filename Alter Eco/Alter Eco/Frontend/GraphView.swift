639136f9c251a3edab96a4895fd6e8c92a4213de
Michele Asara <ma7914@ic.ac.uk>
2020-05-21 18:59:39 +0100

    started refactoring graph view

--- Alter Eco/Alter Eco/Frontend/GraphView.swift
+++ Alter Eco/Alter Eco/Frontend/GraphView.swift
@@ -4,9 +4,9 @@ import SwiftUI
 struct GraphView: View {
     //Two picker variables set to 0 and the values are changed upon the users touch (changed values are found in the code below)
     //The following picker represents the options of 'day' 'week' 'month' 'year'
-    @State var pickerSelectedItem = 0
+    @State var timePickerSelection = 0
     //The following picker represents the travel options of 'all' 'car' 'walk' 'train' 'plane'
-    @State var pickerSelectedTwoItem = 0
+    @State var transportPickerSelection = 0
     @EnvironmentObject var dataGraph : DataGraph
 
     var body: some View {
@@ -14,43 +14,54 @@ struct GraphView: View {
         
         return VStack {
 
-            Picker(selection: $pickerSelectedItem.animation(), label: Text("")) {
-                Text(DataParts.day.name).tag(0)
-                Text(DataParts.week.name).tag(5)
-                Text(DataParts.month.name).tag(10)
-                Text(DataParts.year.name).tag(15)
-            }
-              .pickerStyle(SegmentedPickerStyle())
-              .padding()
+            getTimePicker()
             ZStack{
                 //Gridlines (as declared in gridlines.swift) dynamically change depending on the max value for the view. The value of the sum of the pickers is passed to the gridlines to ensure they adjust for the view.
-                Gridlines(value:self.pickerSelectedItem+self.pickerSelectedTwoItem)
+                Gridlines(value:self.timePickerSelection+self.transportPickerSelection)
                 //The bar chart is constructed here
                 HStack {//The bar displayed depends on the two pickers chosen
-                    ForEach(0..<dataGraph.data[pickerSelectedItem+pickerSelectedTwoItem].carbonByDate.count, id: \.self)
-                    { i in
-                        BarView(height: self.dataGraph.data[self.pickerSelectedItem+self.pickerSelectedTwoItem].carbonByDate[i].carbon,label: self.dataGraph.data[self.pickerSelectedItem+self.pickerSelectedTwoItem].carbonByDate[i].day.shortName,wid: self.pickerSelectedItem)}}
-                
+                    ForEach(0..<dataGraph.data[timePickerSelection+transportPickerSelection].carbonByDate.count, id: \.self)
+                    {
+                        i in
+                        BarView(height: self.dataGraph.data[self.timePickerSelection+self.transportPickerSelection].carbonByDate[i].carbon,label: self.dataGraph.data[self.timePickerSelection+self.transportPickerSelection].carbonByDate[i].day.shortName,wid: self.timePickerSelection)
+                    }
+                    
+                }
             }
             
             //Transport option picker
-            Picker(selection: $pickerSelectedTwoItem.animation(), label: Image("")) {
+            getTransportPicker()
+        }
+    }
+    
+    func getTimePicker() -> some View {
+        Picker(selection: $timePickerSelection.animation(), label: Text("")) {
+            Text("Daily").tag(0)
+            Text("Weekly").tag(5)
+            Text("Monthly").tag(10)
+            Text("Yearly").tag(15)
+        }
+          .pickerStyle(SegmentedPickerStyle())
+          .padding()
+    }
+    
+    func getTransportPicker() -> some View {
+        Picker(selection: $transportPickerSelection.animation(), label: Image("")) {
             Text("All").tag(0)
             Image(systemName: "car").tag(1)
             Image(systemName: "person").tag(2)
             Image(systemName: "tram.fill").tag(3)
             Image(systemName: "airplane").tag(4)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding()
         }
+        .pickerStyle(SegmentedPickerStyle())
+        .padding()
     }
 }
 
 
 struct GraphView_Previews: PreviewProvider {
     static var previews: some View {
-        GraphView()
+        GraphView().environmentObject(DataGraph())
     }
 }
 
