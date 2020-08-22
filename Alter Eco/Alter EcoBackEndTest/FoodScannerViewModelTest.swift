04c5ae7e2ff9a86df357f3ff0b9545df3a2de976
Michele Asara <ma7914@ic.ac.uk>
2020-08-22 20:27:41 +0100

    completed tests for food scanner view model

--- Alter Eco/Alter EcoBackEndTest/FoodScannerViewModelTest.swift
+++ Alter Eco/Alter EcoBackEndTest/FoodScannerViewModelTest.swift
@@ -23,11 +23,16 @@ class FoodScannerViewModelTest: XCTestCase {
         XCTAssert(retrievedDelegate.setCodesRetrievalCallbackArgs.count == 1)
     }
     
+    func simulateScanning(withBarcodes barcodes: Set<String>) {
+        let callback = delegateMock.setCodesRetrievalCallbackArgs.last!
+        // simulate barcode scanning
+        callback(barcodes)
+    }
+    
     func testAllBarcodesAreFetched() {
-        let callback = delegateMock.setCodesRetrievalCallbackArgs.first!
         // simulate barcode scanning
         let barcodes = Set(["123", "456", "789"])
-        callback(barcodes)
+        simulateScanning(withBarcodes: barcodes)
         XCTAssert(retrieverMock.fetchFoodArgs.count == barcodes.count)
         
         for arg in retrieverMock.fetchFoodArgs {
@@ -35,14 +40,68 @@ class FoodScannerViewModelTest: XCTestCase {
         }
     }
     
-    func testFoodsAreSavedWhenRetrieved() {
-        let barcodeCallback = delegateMock.setCodesRetrievalCallbackArgs.first!
-        barcodeCallback(Set(["123"]))
+    func testErrorOccursIfNoBarcodes() {
+        XCTAssert(delegateMock.displayErrorAndDismissArgs.isEmpty)
+        simulateScanning(withBarcodes: Set<String>())
+        XCTAssert(delegateMock.displayErrorAndDismissArgs.count == 1)
+    }
+    
+    func testFoodsAreStoredWhenRetrieved() {
+        simulateScanning(withBarcodes: Set(["123"]))
         
         let foodCallback = retrieverMock.fetchFoodArgs.first!.completionHandler
         let food = Food(barcode: "123")
         foodCallback(food, nil)
         XCTAssert(viewModel.retrievedFoods.count == 1)
-        XCTAssert(viewModel.retrievedFoods.first! == food)
+        XCTAssert(viewModel.retrievedFoods.first == food)
+    }
+    
+    func testFoodsAreStoredWhenNotFoundInDB() {
+        simulateScanning(withBarcodes: Set(["123"]))
+        
+        let foodCallback = retrieverMock.fetchFoodArgs.first!.completionHandler
+        foodCallback(nil, .foodNotFound(barcode: "123"))
+        XCTAssert(viewModel.foodsNotFound.count == 1)
+        XCTAssert(viewModel.foodsNotFound.first?.barcode == "123")
+    }
+    
+    func testCriticalRetrievalErrorsAreReported() {
+        simulateScanning(withBarcodes: Set(["123"]))
+
+        let foodCallback = retrieverMock.fetchFoodArgs.first!.completionHandler
+        foodCallback(nil, .httpFailure(localizedDescription: "http failure test"))
+        foodCallback(nil, .network(localizedDescription: "network failure test"))
+        
+        XCTAssert(delegateMock.displayErrorAndDismissArgs.count == 2)
+        XCTAssert(delegateMock.displayErrorAndDismissArgs[0] == "http failure test")
+        XCTAssert(delegateMock.displayErrorAndDismissArgs[1] == "network failure test")
+    }
+    
+    func testCompletionFlagIsSetWhenAllBarcodesHaveBeenLookedUp() {
+        let barcodes = Set(["123", "456", "789"])
+        simulateScanning(withBarcodes: barcodes)
+        
+        let foodCallback = retrieverMock.fetchFoodArgs.first!.completionHandler
+        for barcode in barcodes {
+            XCTAssertFalse(viewModel.retrievalCompleted)
+            foodCallback(Food(barcode: barcode), nil)
+        }
+        XCTAssert(viewModel.retrievalCompleted)
+    }
+    
+    func testStateCanBeReset() {
+        let barcodes = Set(["123", "456", "789"])
+        simulateScanning(withBarcodes: barcodes)
+        
+        let foodCallback = retrieverMock.fetchFoodArgs.first!.completionHandler
+        barcodes.forEach { foodCallback(Food(barcode: $0), nil) }
+        
+        viewModel.reset()
+        
+        XCTAssertFalse(viewModel.retrievalCompleted)
+        XCTAssert(viewModel.retrievedFoods.isEmpty)
+        XCTAssert(viewModel.foodsNotFound.isEmpty)
+        XCTAssert((viewModel.getScannerDelegate() as? ScannerDelegateMock) != delegateMock)
+        
     }
 }
